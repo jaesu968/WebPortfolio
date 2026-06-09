6cd151ce7331d94d15cb07225d9be9e96f656fa1
jaesu968 <mazterk968@gmail.com>
2026-06-09 13:42:43 -0500

    fixed resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,12 @@
-import React from 'react'; 
-// import modal from './components/Modal.jsx';
-import modal from './Modal.jsx'; // Importing the Modal component to use for displaying media content in the Resume section
+import React from 'react';
+import resumePdf from '../assets/Resume/KJA_AndroidDevResumeJune2026.pdf';
 
 
 const Resume = () => {
     return (
         <section className="resume">
             <h2>Resume</h2>
-            <a href="src/assets/Resume/KJA_AndroidDevResumeJune2026.pdf" target="_blank" rel="noopener noreferrer">
+            <a href={resumePdf} target="_blank" rel="noopener noreferrer">
                 <button>View Resume</button>
             </a>
         </section>
