4b82faf005c6c996e55cd65cc310e10adafdaacd
jaesu968 <mazterk968@gmail.com>
2026-06-14 16:05:26 -0500

    updated resume added

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import resumePdf from '../assets/Resume/KJA_AndroidDevResumeJune2026.pdf';
+import resumePdf from '../assets/Resume/KJA_AndroidDevResumeJune2026Updated.pdf';
 
 
 const Resume = () => {
